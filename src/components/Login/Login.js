92b2b155b3b41188c67a7b15a56dba2f836b4360
HadassahMary <hadassahmj01@gmail.com>
2022-06-08 09:16:41 +0400

    changed to react-hook-form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,112 +5,97 @@ import React, {
   useRef,
   useState,
 } from "react";
+import { useForm } from "react-hook-form";
 import classes from "./Login.module.css";
 import AuthContext from "../../context/auth-context";
 import Button from "../UI/Button/Button";
 import LoginInputField from "./LoginInputField";
 
-const emailReducer = (state, action) => {
-  if (action.type === "USER_INPUT") {
-    return { value: action.value, isValid: action.value.includes("@") };
-  } else if (action.type === "USER_BLUR") {
-    return { value: state.value, isValid: state.value.includes("@") };
-  }
-  return { value: "", isValid: null };
-};
-const passwordReducer = (state, action) => {
-  if (action.type === "USER_INPUT") {
-    return { value: action.value, isValid: action.value.length > 3 };
-  } else if (action.type === "USER_BLUR") {
-    return { value: state.value, isValid: state.value.length > 3 };
-  }
-  return { value: "", isValid: null };
-};
-
 const Login = (props) => {
   const authCtx = useContext(AuthContext);
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const [emailState, handleEmail] = useReducer(emailReducer, {
-    value: "",
-    isValid: null,
-  });
-  const [passwordState, handlePassword] = useReducer(passwordReducer, {
-    value: "",
-    isValid: null,
-  });
   const [isFromValid, setFormValidity] = useState(false);
-
-  useEffect(() => {
-    const interval = setTimeout(() => {
-      const check = emailState.isValid && passwordState.isValid;
-
-      if (check) {
-        setFormValidity(true);
-      } else {
-        setFormValidity(false);
-      }
-    }, 500);
-
-    return () => {
-      clearTimeout(interval);
-    };
-  }, [emailState.isValid, passwordState.isValid]);
-
-  const emailChangeHandler = (event) => {
-    handleEmail({ type: "USER_INPUT", value: event.target.value });
-  };
-  const isEmailValid = () => {
-    handleEmail({ type: "USER_BLUR" });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    authCtx.onlogin(emailRef.current.value);
-  };
-  const passwordChangeHandler = (event) => {
-    handlePassword({ type: "USER_INPUT", value: event.target.value });
-  };
-  const isPasswordValid = () => {
-    handlePassword({ type: "USER_BLUR" });
-  };
   
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      FirstName: "",
+      Email: "",
+      Password: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    authCtx.onlogin(data.Email);
+    setFormValidity(true);
+  };
 
   return (
-    <div className={`${classes.login} ${classes.card}`} >
-        <h2>Login Form</h2>
-      <form onSubmit={handleSubmit} noValidate>
+    <div className={`${classes.login} ${classes.card}`}>
+      <h2>Login Form</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div
           className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
+            errors.FirstName?.message === "" ? classes.invalid : ""
           }`}
         >
-          <LoginInputField
-            label="Email"
-            type="email"
-            onChange={emailChangeHandler}
-            onBlur={isEmailValid}
-            value={emailState.value}
-            passref={emailRef}
+          <input
+            type="text"
+            placeholder="First Name"
+            {...register("FirstName", {
+              required: "This field is required",
+              minLength: {
+                value: 5,
+                message: "The length should be atleast 5 characters",
+              },
+            })}
           />
         </div>
-        {emailState.isValid===false && <p>Should contain '@'.</p>}
+
         <div
           className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ""
+            errors.Email?.message === "" ? classes.invalid : ""
           }`}
         >
-          <LoginInputField
-            label="Password"
+          <p>{errors.FirstName?.message}</p>
+          <input
+            type="text"
+            placeholder="Email"
+            {...register("Email", {
+              required: "This field is required",
+              pattern: {
+                value: /^\S+@\S+$/i,
+                message: "Please enter a valid email.",
+              },
+              minLength: { value: 5, message: "The email is too short." },
+            })}
+          />
+          <p>{errors.Email?.message}</p>
+        </div>
+        <div
+          className={`${classes.control} ${
+            errors.Password?.message === "" ? classes.invalid : ""
+          }`}
+        >
+          <input
             type="password"
-            onChange={passwordChangeHandler}
-            onBlur={isPasswordValid}
-            value={passwordState.value}
-            passref={passwordRef}
+            placeholder="Password"
+            {...register("Password", {
+              required: "This field is required",
+              minLength: {
+                value: 4,
+                message: "The length should be atleast 4 characters",
+              },
+            })}
           />
+          <p>{errors.Password?.message}</p>
         </div>
-        {passwordState.isValid===false && <p>Length should be more than 3.</p>}
-        <Button type="submit" disabled={!isFromValid}></Button>
+        <div className={classes.buttonHolder}>
+          <Button type="submit"></Button>
+        </div>
       </form>
     </div>
   );
